de6b704b67e5c2fe4d749694eac3a8402216e6f9
Virtuouz <giovanni_plascencia@hotmail.com>
2023-04-06 07:27:20 -0600

    5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
 const sumAll = function() {
+    args=Array.from(arguments)
+    sum=0;
+    for(val of args){
+        if(typeof(val)!=="number" || val<0)
+        return "ERROR"
+    }
+    rangeStart=args[0]
+    rangeEnd=args[2]
+    if(rangeStart>rangeEnd){
+        temp=rangeStart
+        rangeStart=rangeEnd
+        rangeEnd=temp
+    }
+    
 
 };
 
