308edac6ec111057e519476531d5c78383c2b2a3
Giovanni N Plascencia <GPlascencia@csi.edu>
2023-04-06 16:50:46 -0600

    doners

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
 const sum = function() {
-	
+	vals=Array.from(arguments[0]).map(x=>parseInt(x,10))
+  console.log(vals)
+  summation=0;
+  for(val of vals){
+    summation+=val;
+  }
+  return summation
 };
 
 const multiply = function() {
-
+  vals=Array.from(arguments[0]).map(x=>parseInt(x,10))
+  product=0;
+  if(vals.length>0)
+  product=1;
+  for(val of vals){
+    product*=val
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(num1,  num2) {
+	return num1**num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0)
+  return 1;
+  if(num===1)
+  return 1
+
+  return num*factorial(num-1)
 };
+
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word=word.replace(/[^a-zA-Z]+/g, '')
+    word=word.toUpperCase();
+    backwards=word.split("").reverse().join("");
+    console.log(word)
+    console.log(backwards)
+    if(word==backwards){
+        return true
+    }
+    return false;
 };
 
+palindromes("ZZZZ car, a man, a maracaz.")
 // Do not edit below this line
 module.exports = palindromes;
